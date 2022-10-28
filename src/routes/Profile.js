b6c9ab907502d505a9d7dc2cdecdf655569f96fb
hanchorong <hancrong@gmail.com>
2022-10-28 14:22:49 +0900

    logOut

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,20 @@
+import { authService } from "fBase";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export default () => <span>Profile</span>;
+const Profile = () => {
+  const history = useNavigate();
+
+  const onLogOutClick = () => {
+    authService.signOut();
+    history("/");
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
+
+export default Profile;
